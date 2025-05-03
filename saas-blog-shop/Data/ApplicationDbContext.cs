6c9a4f8e4bcb563936eb78753ec103efc790dd86
Kung'u <67824027+KariukiKungu@users.noreply.github.com>
2025-05-03 17:47:37 +0300

    Add saas-blog-shop/Data/ApplicationDbContext.cs 1746193549854

---
+++ saas-blog-shop/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using saas_blog_shop.Models;
+
+namespace saas_blog_shop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
