bbf5f0efe5a6e442db388e1bc3e13e7690a8a5ed
Kung'u <67824027+KariukiKungu@users.noreply.github.com>
2025-05-03 17:47:35 +0300

    Add saas-blog-shop/Models/ApplicationUser.cs 1746193547849

---
+++ saas-blog-shop/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace saas_blog_shop.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Additional properties can be added here for SaaS user profile
+    }
+}
